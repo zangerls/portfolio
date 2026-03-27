39c303ef0daee1bd84f65fbd5be389b7caff06fa
Simon Zangerl <simon.zangerl@hotmail.com>
2026-03-27 11:22:58 +0100

    Created basic container component

---
+++ components/container.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
+
+export function Container({
+  children,
+  withBorder,
+}: {
+  children: ReactNode
+  withBorder?: boolean
+}) {
+  return (
+    <div className={cn(withBorder && "border-x", "mx-4 lg:mx-56")}>
+      {children}
+    </div>
+  )
+}
