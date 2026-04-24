42c6b2097ece05371841614fe60cfe9174ef0768
Simon Zangerl <simon.zangerl@hotmail.com>
2026-04-24 19:20:28 +0200

    Aria labels for theme toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -8,14 +8,15 @@ export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
   return (
     <div className="flex items-center gap-2">
-      <button onClick={() => setTheme("dark")}>
+      <button onClick={() => setTheme("dark")} aria-label="Toggle dark-mode">
         <IconMoon size="1.1rem" />
       </button>
       <Switch
+        aria-label="Toggle color scheme"
         checked={theme === "light"}
         onCheckedChange={(e) => setTheme(e ? "light" : "dark")}
       />
-      <button onClick={() => setTheme("light")}>
+      <button onClick={() => setTheme("light")} aria-label="Toggle light-mode">
         <IconSun size="1.1rem" />
       </button>
     </div>
